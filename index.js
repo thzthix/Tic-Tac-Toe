a08cf99a9622729127439c8305d024df30687f56
thzthix <95135403+thzthix@users.noreply.github.com>
2023-03-21 00:39:55 +0900

    updated showing  messages

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const resetBtn = document.querySelector(".reset-btn");
-const h2 = document.querySelector("h2");
-const displayTurnSpan = h2.querySelector(".turn");
+const messageContainer = document.querySelector(".message-container");
+const occupiedMessage = messageContainer.querySelector(".message-occupied");
+const wonMessage = messageContainer.querySelector(".message-won");
+const messageTurn = messageContainer.querySelector(".message-turn");
+const turnSpanEl = messageTurn.querySelector(".turn");
+const winnerSpanEl = wonMessage.querySelector(".winner");
+const messageDraw = messageContainer.querySelector(".message-draw");
 
-const PLAYER = { name: "player", mark: "O" };
-const COMP = { name: "computer", mark: "X" };
+const PLAYER = {
+  name: "player",
+  mark: `<i class="icon fa-regular fa-circle"></i>`,
+
+  // };
+};
+const COMP = { name: "computer", mark: `<i class="icon fa-solid fa-x"></i>` };
 const board = {
   cells: [
     "cell-1",
@@ -18,7 +28,7 @@ const board = {
   ],
   cellIdPrefix: "cell-",
   display: document.querySelector(".board"),
-  emptyCell: [
+  emptyCell: new Set([
     "cell-1",
     "cell-2",
     "cell-3",
@@ -28,55 +38,64 @@ const board = {
     "cell-7",
     "cell-8",
     "cell-9",
-  ],
-  resetBoard: function () {
-    this.emptyCell = [];
-    this.cells.forEach((cell) => {
-      this.emptyCell.push(cell);
-    });
-    console.log("emptycells:::::::::::::");
-    console.log(`cells:${this.emptyCell}`);
-    const cells = document.getElementsByClassName("cell");
+  ]),
 
-    Array.from(cells).forEach((cell) => {
-      if (cell.innerText) {
-        cell.innerText = "";
+  resetBoard: function () {
+    this.emptyCell = new Set([
+      "cell-1",
+      "cell-2",
+      "cell-3",
+      "cell-4",
+      "cell-5",
+      "cell-6",
+      "cell-7",
+      "cell-8",
+      "cell-9",
+    ]);
+
+    for (const cell of this.emptyCell) {
+      const cellEl = document.getElementById(cell);
+      const cellElClasses = cellEl.classList;
+
+      for (let i = 0; i < cellElClasses.length; i++) {
+        const className = cellElClasses[i];
+        if (className.startsWith("marked")) {
+          cellElClasses.remove(className);
+        }
       }
-    });
+      cellEl.innerHTML = "";
+    }
   },
   diagnols: [
     ["cell-1", "cell-5", "cell-9"],
     ["cell-3", "cell-5", "cell-7"],
   ],
-  markCell: function (markedCell, mark) {
-    markedCell.textContent = mark;
-    const index = this.emptyCell.indexOf(markedCell.id);
-    if (index > -1) {
-      // only splice array when item is found
-      this.emptyCell.splice(index, 1); // 2nd parameter means remove one item only
+  markCell: function (id, mark) {
+    const markedCell = document.getElementById(`${id}`);
+
+    markedCell.innerHTML = mark;
+    markedCell.classList.add(`marked--${turn}`);
+
+    if (this.emptyCell.delete(markedCell.id)) {
+      markedCell.classList.add(`marked--${turn}`);
     }
   },
   getRandomCellId: function () {
-    const randomIndex = Math.floor(Math.random() * this.emptyCell.length);
+    const randomIndex = Math.floor(Math.random() * this.emptyCell.size);
+    const emptySetArray = Array.from(this.emptyCell);
 
-    const cellId = this.emptyCell[randomIndex];
+    const cellId = emptySetArray[randomIndex];
     return cellId;
   },
-  getRandomCell: function () {
-    const randomCellId = this.getRandomCellId();
-    const computerMove = document.getElementById(`${randomCellId}`);
-    return computerMove;
-  },
 };
-
+let turn = PLAYER.name;
 let compTimeoutId;
 
 //1:occupied 0:not occupied
 let isPlaying = false;
-let turn = PLAYER.name;
+
 function areCellsEqual(...cells) {
-  const firstCell = cells[0];
-  return cells.every((cell) => cell.textContent === firstCell.textContent);
+  return cells.every((cell) => cell.classList.contains(`marked--${turn}`));
 }
 function checkForSameMarksInRow(parentCell) {
   const cellsOfRow = parentCell.children;
@@ -99,14 +118,12 @@ function checkForSameMarksInColumn(cell) {
   console.log("column is not same");
   return false;
 }
-function checkForSameMarksIndiagonal(diagnol) {
-  const firstCell = document.querySelector(`#${diagnol[0]}`);
-  const secondCell = document.querySelector(`#${diagnol[1]}`);
-  const thirdCell = document.querySelector(`#${diagnol[2]}`);
-  if (
-    firstCell.textContent === secondCell.textContent &&
-    secondCell.textContent === thirdCell.textContent
-  ) {
+function checkForSameMarksIndiagonal(...diagnol) {
+  const diagnols = [];
+  diagnol.forEach((cellId) => {
+    diagnols.push(document.getElementById(cellId));
+  });
+  if (areCellsEqual(...diagnols)) {
     console.log("diagnol is same");
     return true;
   }
@@ -120,7 +137,7 @@ function checkForWin(cell, rowContainer) {
     checkForSameMarksInColumn(cell) ||
     board.diagnols.some(
       (diagnol) =>
-        diagnol.includes(cellId) && checkForSameMarksIndiagonal(diagnol)
+        diagnol.includes(cellId) && checkForSameMarksIndiagonal(...diagnol)
     )
   ) {
     return true;
@@ -128,16 +145,22 @@ function checkForWin(cell, rowContainer) {
   return false;
 }
 function displayWinner(winner) {
-  h2.innerText = `${winner} won!`;
+  messageTurn.classList.add("hide");
+  wonMessage.classList.remove("hide");
+  winnerSpanEl.textContent = `${winner}`;
 }
 function displayTurn(turn) {
-  displayTurnSpan.innerText = turn;
+  turnSpanEl.textContent = turn;
 }
 function changeTurn() {
   if (turn === PLAYER.name) {
-    board.display.addEventListener("click", onClickCell);
+    if (board.display.classList.contains("disabled")) {
+      board.display.classList.remove("disabled");
+    }
+    // board.display.addEventListener("click", onClickCell);
   } else if (turn === COMP.name) {
-    board.display.removeEventListener("click", onClickCell);
+    board.display.classList.add("disabled");
+    // board.display.removeEventListener("click", onClickCell);
     clearTimeout(compTimeoutId);
     compTimeoutId = setTimeout(computerTurn, 1000);
   }
@@ -155,6 +178,8 @@ function handleturnResult(ifWon) {
   }
   const isDrawState = isDraw();
   if (isDrawState) {
+    isPlaying = false;
+    messageDraw.classList.remove("hide");
     changeResetBtnText();
     turn === PLAYER.name &&
       board.display.removeEventListener("click", onClickCell);
@@ -167,56 +192,82 @@ function onClickCell(event) {
   console.log("clicked!!!!!!!");
   const cellClicked = event.target;
   const rowContainer = event.target.parentNode;
+  if (
+    cellClicked.classList.contains("icon") ||
+    cellClicked.classList.contains("marked--player") ||
+    cellClicked.classList.contains("marked--computer")
+  ) {
+    occupiedMessage.classList.remove("hide");
+    return;
+  }
   if (!event.target.classList.contains("cell")) {
     return;
   }
-  if (cellClicked.textContent) {
-    alert("This cell is already occupied. Please select another cell.");
-    return;
-  }
 
-  board.markCell(cellClicked, PLAYER.mark);
+  if (
+    !occupiedMessage.classList.contains("hide") &&
+    !cellClicked.classList.contains("icon")
+  ) {
+    occupiedMessage.classList.add("hide");
+  }
+  board.markCell(event.target.id, PLAYER.mark);
   const ifWon = checkForWin(cellClicked, rowContainer);
   handleturnResult(ifWon);
 }
 
 function computerTurn() {
-  let computerMove = board.getRandomCell();
-  console.log(computerMove);
-  board.markCell(computerMove, COMP.mark);
+  let computerMoveId = board.getRandomCellId();
+  console.log(computerMoveId);
+  board.markCell(computerMoveId, COMP.mark);
+
+  const computerMove = document.getElementById(`${computerMoveId}`);
   const rowContainer = computerMove.parentNode;
 
   const ifWon = checkForWin(computerMove, rowContainer);
   handleturnResult(ifWon);
 }
 function isDraw() {
-  if (board.emptyCell.length === 0) {
+  if (board.emptyCell.size === 0) {
     isPlaying = false;
-    h2.textContent = "DRAW!";
+    messageDraw.classList.remove("hide");
+    messageTurn.classList.add("hide");
     return true;
   } else {
     return false;
   }
 }
 function changeResetBtnText() {
-  resetBtn.innerText = "play again";
+  resetBtn.textContent = "play again";
+  resetBtn.classList.add("btn-playAgain");
 }
 function resetGame() {
   if (compTimeoutId) {
     clearTimeout(compTimeoutId);
   }
   board.resetBoard();
+  board.display.classList.remove("disabled");
+  resetBtn.classList.remove("btn-playAgain");
+  resetBtn.textContent = "reset";
 
   if (turn === COMP.name) {
     turn = PLAYER.name;
   }
-
+  if (!occupiedMessage.classList.contains("hide")) {
+    occupiedMessage.classList.add(hide);
+  }
+  if (!wonMessage.classList.contains("hide")) {
+    wonMessage.classList.add("hide");
+    messageTurn.classList.remove("hide");
+  } else if (!messageDraw.classList.contains("hide")) {
+    messageDraw.classList.add("hide");
+    messageTurn.classList.remove("hide");
+  }
   playGame();
 }
 
 function playGame() {
   isPlaying = true;
-  cellsOccupyState = 0;
+
   resetBtn.addEventListener("click", resetGame);
   board.display.addEventListener("click", onClickCell);
 }
